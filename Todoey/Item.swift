56befa7fa56f5ccae284e056e2a62cccd476e061
Takumi Sawano <aa0129456@yahoo.co.jp>
2019-05-11 21:41:10 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 沢野拓実 on 2019/05/10.
+//  Copyright © 2019 沢野拓実. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
